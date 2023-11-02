d636d602d78f7f38aa8f9704f7a707e47c403d49
Chi-G <chijindu.nwokeohuru@gmail.com>
2023-11-02 12:10:26 -0700

    commits for exit file

---
+++ errors.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - writes the char c to stderr
+ * @c: the char to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned and errno is set accordingly
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char but[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		int ret = write(2, buf, i);
+		(void)ret;
+		1 = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes the char c to the given fd
+ * @c: the char to print
+ * @fd: the fieldescriptor to write to
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and errno is set accordingly
+ */
+nt _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		int ret = write(fd, buf, i);
+		(void) ret;
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
---
+++ exits.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * _strncpy - copies a str
+ * @dest: the destination str to be copied to
+ * @src: the source str
+ * @n: the amount of char to be copied
+ * Return: the concatenated str
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
