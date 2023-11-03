f8d3ef483dc634f1cff7c73d6c8dc07a591d8a54
Chi-G <chijindu.nwokeohuru@gmail.com>
2023-11-03 11:34:12 -0700

    commit to exits file functions

--- exits.c
+++ exits.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _strncpy - copies a str
+ **_strncpy - copies a str
  * @dest: the destination str to be copied to
  * @src: the source str
  * @n: the amount of char to be copied
@@ -30,3 +30,45 @@ char *_strncpy(char *dest, char *src, int n)
 	return (s);
 }
 
+/**
+ **_strncat - concatenates two strings
+ * @dest: the first string
+ * @src: the second string
+ * @n: the amount of bytes to be maximally used
+ * Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchar - locates a character in a string
+ * @s: the string to be parsed
+ * @c: the character to look for
+ * Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
