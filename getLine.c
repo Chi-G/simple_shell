ccfa48145c2a23422fe8d23b1a8ae6a53ccc1587
Chi-G <chijindu.nwokeohuru@gmail.com>
2023-11-13 03:06:39 -0800

    commit updates

--- getLine.c
+++ getLine.c
@@ -22,7 +22,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 		r = getline(buf, &len_p, stdin);
 #else
 		r = _getline(info, buf, &len_p);
-#elseif
+#endif
 		if (r > 0)
 		{
 			if ((*buf)[r - 1] == '\n')
@@ -32,7 +32,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 			}
 			info->linecount_flag = 1;
 			remove_comments(*buf);
-			build_history_list(info, *buf, info->historycount++);
+			build_history_list(info, *buf, info->histcount++);
 			{
 				*len = r;
 				info->cmd_buf = buf;
